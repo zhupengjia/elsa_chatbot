45bdd35473c4417c1531256fc6d2dbaf6bd4e808
Pengjia Zhu <zhupengjia@gmail.com>
2018-03-08 10:07:58 +0800

    updated

--- src/model/dialog_tracker.py
+++ src/model/dialog_tracker.py
@@ -26,8 +26,8 @@ class Dialog_Tracker(Model_Base):
         self.fc_response1 = nn.Linear(self.Nresponses, self.cfg['fc_response1'])
         self.fc_response2 = nn.Linear(self.cfg['fc_response1'], self.cfg['fc_response2'])
         fc1_input_size = self.cfg['cnn_kernel_num']*2 + self.cfg['max_entity_types'] + self.cfg['fc_response2']
-        self.fc_dialog = nn.Linear(fc1_input_size, self.cfg['dialog_emb_size'])
-        self.lstm = nn.LSTM(self.cfg['dialog_emb_size'], self.Nresponses)
+        self.fc_dialog = nn.Linear(fc1_input_size, self.Nresponses)
+        self.lstm = nn.LSTM(self.Nresponses, self.Nresponses, num_layers=1)
         self.softmax = nn.Softmax(dim=1)
         self.lstm_hidden = self.init_hidden()
 
@@ -44,8 +44,12 @@ class Dialog_Tracker(Model_Base):
         return x
 
     def init_hidden(self):
-        return (autograd.Variable(torch.zeros(1, 1, self.Nresponses)),
-                autograd.Variable(torch.zeros(1, 1, self.Nresponses)))
+        if self.cfg.use_gpu:
+            return (autograd.Variable(torch.zeros(1, 1, self.Nresponses).cuda(self.cfg.use_gpu-1)),\
+                    autograd.Variable(torch.zeros(1, 1, self.Nresponses).cuda(self.cfg.use_gpu-1)))
+        else:
+            return (autograd.Variable(torch.zeros(1, 1, self.Nresponses)),\
+                   autograd.Variable(torch.zeros(1, 1, self.Nresponses)))
         
 
     def dialog_embedding(self, utterance, entity,  response_prev):
@@ -58,15 +62,16 @@ class Dialog_Tracker(Model_Base):
         response_prev = self.responseencoder(response_prev)
         #concat together and apply linear
         utter = torch.cat((utter_att, entity, response_prev), 1)
-        utter = self.fc_dialog(utter) 
-        return self.softmax(utter)
+        return self.fc_dialog(utter) 
 
     def get_response(self, dialog):
         #first get dialog embedding
         dialog_emb = self.dialog_embedding(dialog['utterance'], dialog['entity'], dialog['response_prev'])
         #dialog embedding to lstm as dialog tracker
         lstm_out, _ = self.lstm(dialog_emb.view(len(dialog_emb),1,-1), self.lstm_hidden)
+        lstm_out = self.dropout(lstm_out)
         lstm_out = lstm_out.view(len(dialog_emb), -1)
+        #lstm_out = dialog_emb
         #output to softmax
         lstm_softmax = self.softmax(lstm_out)
         #apply mask 
