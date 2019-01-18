4223fa3d127e5af7e98f42079e9608f50779254d
Pengjia Zhu <zhupengjia@gmail.com>
2019-01-18 21:54:12 +1300

    updated

--- src/model/policy_gradiant.py
+++ src/model/policy_gradiant.py
@@ -13,26 +13,54 @@ import torch.optim as optim
 '''
 
 class Policy_Gradiant:
-    def __init__(self, cfg_fn, reader_cls, hook_cls):
+    def __init__(self, adversal_chatbot, reader, bert_model_name, max_entity_types, fc_responses=5, entity_layers=2, lstm_layers=1, hidden_dim=300, epochs=1000, weight_decay=0, learning_rate=0.001, saved_model="model.pt", dropout=0.2, device=None, logger=None):
         '''
             Policy Gradiant for end2end chatbot
 
             Input:
-                - cfg_fn: config file location
-                - reader_class: class for reader
-                - hook_cls: class for hook
+                - adversal_chatbot: adversal chatbot instance
+                - bert_model_name: bert model file location or one of the supported model name
+                - reader: reader instance, see ..module.reader.*.py
+                - max_entity_types: int, number of entity types 
+                - fc_responses: int
+                - entity_layers: int, default is 2
+                - lstm_layers: int, default is 1
+                - hidden_dim: int, default is 300
+                - epochs: int, default is 1000
+                - weight_decay: int, default is 0
+                - learning_rate: float, default is 0.001
+                - saved_model: str, default is "model.pt"
+                - dropout: float, default is 0.2
+                - device: instance of torch.device, default is cpu device
+                - logger: logger instance ,default is None
         '''
         self.cfg = Config(cfg_fn)
         if not torch.cuda.is_available(): 
             self.cfg.model.use_gpu = 0
-        self.logger = setLogger(self.cfg.logger)
-        
-        self.__init_adversary(hook_cls)
-        self.__init_reader(reader_cls)
+       
+        self.reader = reader
+        self.bert_model_name = bert_model_name
+        self.saved_model = saved_model
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+        self.epochs = epochs
+        self.device = device
+        self.logger = logger 
+
+        self.max_entity_types = max_entity_types
+        self.fc_responses = fc_responses
+        self.lstm_layers = lstm_layers
+        self.entity_layers = entity_layers
+        self.hidden_dim = hidden_dim
+        self.dropout = dropout
+
+        self.ad_chatbot = adversal_chatbot
+        self.clientid = 'DLTRAIN'
+
         self.__init_tracker()
 
     @classmethod
-    def build(cls, config, reader_cls, hook):
+    def build(cls, config, reader_cls, hook, adversal_hook):
         '''
             construct model from config
 
@@ -40,6 +68,8 @@ class Policy_Gradiant:
                 - config: configure dictionary
                 - reader_cls: class for reader
                 - hook: hook instance, please check src/hook/babi_gensays.py for example
+                - adversal_hook: adversal hook instance, please check src/hook/babi_gensays.py for example
+
         '''
         logger = setLogger(**config.logger)        
 
@@ -55,37 +85,26 @@ class Policy_Gradiant:
         reader.build_responses(config.response_template) #build response template index, will read response template and create entity need for each response template every time, but not search index.
         reader.response_dict.build_mask()
 
-        return cls(reader=reader, logger=logger, bert_model_name=config.tokenizer.bert_model_name, max_entity_types=config.entity_dict.max_entity_types, fc_responses=config.model.fc_responses, entity_layers=config.model.entity_layers, lstm_layers=config.model.lstm_layers, hidden_dim=config.model.hidden_dim, epochs=config.model.epochs, weight_decay=config.model.weight_decay, learning_rate=config.model.learning_rate, saved_model=config.model.saved_model, dropout=config.model.dropout, device=device)
+        #adversal chatbot
+        ad_chatbot = Rule_Based.build(config.rule_based, adversal_hook)
 
-    
-
-    def __init_adversary(self, hook_cls):
-        '''adversary chatbot'''
-        hook = hook_cls(self.cfg)
-        self.ad_chatbot = Rule_Based(self.cfg.rule_based, hook)
-        self.clientid = 'DLTRAIN' 
+        return cls(adversal_chatbot = ad_chatbot, reader=reader, logger=logger, bert_model_name=config.tokenizer.bert_model_name, max_entity_types=config.entity_dict.max_entity_types, fc_responses=config.model.fc_responses, entity_layers=config.model.entity_layers, lstm_layers=config.model.lstm_layers, hidden_dim=config.model.hidden_dim, epochs=config.model.epochs, weight_decay=config.model.weight_decay, learning_rate=config.model.learning_rate, saved_model=config.model.saved_model, dropout=config.model.dropout, device=device)
 
 
     def __init_tracker(self):
         '''tracker'''
-        self.tracker =  Dialog_Tracker(self.cfg.model, self.reader.vocab, len(self.reader))
-        self.tracker.network()
+        self.tracker =  Dialog_Tracker(bert_model_name=self.bert_model_name, Nresponses=len(self.reader), max_entity_types=self.max_entity_types, fc_responses=self.fc_responses, entity_layers=self.entity_layers, lstm_layers=self.lstm_layers, hidden_dim=self.hidden_dim, dropout=self.dropout)
         
-        #use pretrained word2vec
-        self.tracker.encoder.embedding.weight.data = torch.FloatTensor(self.tracker.vocab.dense_vectors())
-
-        if self.cfg.model.use_gpu:
-            self.tracker.cuda(self.cfg.model.use_gpu-1)
+        self.tracker.to(self.device)
 
         #checkpoint
-        if os.path.exists(self.cfg.model['saved_model']):
-            if self.cfg.model.use_gpu:
-                checkpoint = torch.load(self.cfg.model['saved_model'])
-            else:
-                checkpoint = torch.load(self.cfg.model['saved_model'], map_location={'cuda:0': 'cpu'})
+        if os.path.exists(self.saved_model):
+            checkpoint = torch.load(self.saved_model, map_location={'cuda:0': self.device.type})
             self.tracker.load_state_dict(checkpoint['model'])
         
-        self.optimizer = optim.Adam(self.tracker.parameters(), lr=self.cfg.model.learning_rate, weight_decay=self.cfg.model.weight_decay)
+
+        self.loss_function = torch.nn.NLLLoss()
+        self.optimizer = optim.Adam(self.tracker.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
 
 
     def __memory_replay(self):
