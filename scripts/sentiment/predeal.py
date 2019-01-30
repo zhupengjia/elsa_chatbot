56cce3f67935022b186706e33760d6a4f2f49360
Pengjia Zhu <zhupengjia@gmail.com>
2019-01-30 23:50:26 +1300

    updated for using hdf5 for sentiment training

---
+++ scripts/sentiment/predeal.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import glob, os, gzip, shutil, json
+import h5py, numpy
+from nlptools.text  import Tokenizer
+
+max_seq_len = 200
+bert_model_name = "/home/pzhu/data/bert/bert-base-uncased"
+data_folder = "/home/pzhu/data/amazon-reviews-pds/"
+db_path = "/home/pzhu/data/amazon-reviews-pds/reviews.h5"
+
+data_paths = glob.glob(os.path.join(data_folder, "*.json.gz"))
+
+#num_lines = 0
+#for d in data_paths:
+#    with gzip.open(d, 'rb') as f:
+#        num_lines += sum(1 for l in f)
+#print(num_lines)
+num_lines = 18186733
+
+if os.path.exists(db_path):
+    os.remove(db_path)
+
+tokenizer = Tokenizer(tokenizer='bert', bert_model_name=bert_model_name)
+vocab = tokenizer.vocab
+def jsonconvert(jsonstr):
+    data = json.loads(jsonstr)
+    text = data['reviewText']
+    rate = int(data["overall"]) - 1
+    text_ids = numpy.zeros(max_seq_len)
+    text_mask = numpy.zeros(max_seq_len)
+
+    temp_ids = vocab(tokenizer(text))[:max_seq_len-2]
+    seq_len = len(temp_ids) + 2
+
+    text_ids[0] = vocab.CLS_ID
+    text_ids[1:seq_len-1]  = temp_ids
+    text_ids[seq_len-1] = vocab.SEP_ID
+    
+    text_mask[:seq_len] = 1
+    return text_ids, text_mask, rate
+
+
+with h5py.File(db_path, "w") as h5file:
+    id_set = h5file.create_dataset("ids", (num_lines, max_seq_len), dtype='i', chunks=True) 
+    mask_set = h5file.create_dataset("masks", (num_lines, max_seq_len), dtype='i', chunks=True) 
+    label_set = h5file.create_dataset("label", (num_lines,), dtype='i', chunks=True) 
+    h5file.attrs["Nlines"] = num_lines
+    ikey = 0
+    for data_path in data_paths:
+        with gzip.open(data_path, "rb") as f:
+            for l in f:
+                text_ids, text_mask, rate = jsonconvert(l)
+                id_set[ikey] = text_ids
+                mask_set[ikey] = text_mask
+                label_set[ikey] = rate
+                ikey += 1
+                if ikey % 10 == 0:
+                    print(data_path, ikey)
+
---
+++ scripts/sentiment/train.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import glob, numpy, gzip, os, json, torch, sys, h5py
+sys.path.append("../..")
+import torch.nn as nn
+from chatbot_end2end.model.sentence_encoder import Sentence_Encoder
+from chatbot_end2end.model.sentiment import Sentiment
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+
+
+class DataReader(Dataset):
+    def __init__(self, data_path):
+        super().__init__()
+        self.data = h5py.File(data_path, 'r')
+   
+    def __len__(self):
+        return self.data["ids"].shape[0]
+
+    def __getitem__(self, i):
+        return torch.LongTensor(self.data["ids"][i]), torch.LongTensor(self.data["masks"][i]), torch.tensor(self.data["label"][i], dtype=torch.long)
+
+
+class Sentiment_Full(nn.Module):
+    def __init__(self, bert_model_name):
+        super().__init__()
+        self.encoder = Sentence_Encoder(bert_model_name)
+        self.sentiment = Sentiment(self.encoder.hidden_size)
+    
+    def forward(self, sentence, attention_mask, labels=None):
+        _, pooled_output = self.encoder(sentence, attention_mask=attention_mask)
+        return self.sentiment(pooled_output, labels)
+
+
+def train():
+    batch_size = 200
+    epochs = 100
+    learning_rate = 0.001
+    num_workers = 1
+    bert_model_name = "/home/pzhu/data/bert/bert-base-uncased"
+    saved_model = '/home/pzhu/data/amazon-reviews-pds/sentiment.pt'
+    data_path = "/home/pzhu/data/amazon-reviews-pds/reviews_part.h5"
+    
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+    
+    dataset = DataReader(data_path)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    model = Sentiment_Full(bert_model_name).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    
+    
+    if os.path.exists(saved_model):
+        checkpoints = torch.load(saved_model, map_location={'cuda:0':str(device)})
+        model.load_state_dict(checkpoints)
+    
+    for epoch in range(epochs):
+        for it, data in enumerate(dataloader):
+            input_ids = data[0].to(device)
+            mask = data[1].to(device)
+            labels = data[2].to(device)
+            loss = model(input_ids, mask, labels)
+            loss.backward()
+        
+            optimizer.step()
+            optimizer.zero_grad()
+            print('{}/{} loss {}'.format(epoch, it, loss))
+
+            if it>0 and it%1000 == 0:
+                torch.save(model.state_dict(), saved_model)
+                
+        torch.save(model.state_dict(), saved_model)
+
+
+
+if __name__ == "__main__":
+    train()
+    
+
