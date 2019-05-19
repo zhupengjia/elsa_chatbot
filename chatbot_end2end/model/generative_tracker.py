5294332a8ba5315c8c583d79cb19694490e31f7b
Pengjia Zhu <zhupengjia@gmail.com>
2019-05-19 21:15:46 +0800

    updated

--- chatbot_end2end/model/generative_tracker.py
+++ chatbot_end2end/model/generative_tracker.py
@@ -27,10 +27,11 @@ class GenerativeTracker(nn.Module):
             - unk_penalty (float, optional): unknown word penalty, where < 1.0 favors more unks, >1.0 favors less. Used for prediction only. default is 1.0
     '''
     def __init__(self, skill_name, shared_layers=None, model_type="transformer",
-                 bert_model_name="bert-base-uncased", vocab_size=30522, encoder_hidden_layers=12,
-                 encoder_attention_heads=12, max_position_embeddings=512,
-                 encoder_intermediate_size=3072, encoder_hidden_size=768,
-                 decoder_hidden_layers=1, decoder_attention_heads=2, decoder_hidden_size=1024,
+                 bert_model_name="bert-base-uncased", vocab_size=30522,
+                 encoder_hidden_layers=12, encoder_attention_heads=12,
+                 encoder_hidden_size=768, encoder_intermediate_size=2048,
+                 encoder_freeze=False, max_position_embeddings=512, decoder_hidden_layers=1,
+                 decoder_attention_heads=2, decoder_hidden_size=1024,
                  dropout=0, pad_id=0, bos_id=1, eos_id=2, unk_id=3, beam_size=1,
                  len_penalty=1., unk_penalty=1., **args):
         super().__init__()
@@ -41,13 +42,14 @@ class GenerativeTracker(nn.Module):
                                             encoder_hidden_layers= encoder_hidden_layers,
                                             encoder_attention_heads=encoder_attention_heads,
                                             max_position_embeddings=max_position_embeddings,
-                                            encoder_intermediate_size=encoder_intermediate_size,
                                             encoder_hidden_size=encoder_hidden_size,
+                                            encoder_intermediate_size=encoder_intermediate_size,
                                             dropout=dropout)
             if shared_layers is not None:
                 shared_layers["encoder"] = self.encoder
         else:
             self.encoder = shared_layers["encoder"]
+        if encoder_freeze: self.encoder.freeze()
 
         self.response_key = 'response_' + skill_name
         self.mask_key = 'response_mask_' + skill_name
@@ -61,16 +63,17 @@ class GenerativeTracker(nn.Module):
             from nlptools.zoo.encoders.lstm import LSTMDecoder
             self.decoder = LSTMDecoder(self.encoder.embedding,
                                        num_hidden_layers=decoder_hidden_layers,
-                                       intermediate_size=decoder_hidden_size, dropout=dropout)
+                                       dropout=dropout)
         else:
             from nlptools.zoo.encoders.transformer import TransformerDecoder
             self.decoder = TransformerDecoder(self.encoder.embedding,
-                                              num_layers=decoder_hidden_layers,
+                                              num_hidden_layers=decoder_hidden_layers,
                                               num_attention_heads=decoder_attention_heads,
                                               intermediate_size=decoder_hidden_size,
                                               dropout=dropout)
         
         self.config = {"encoder":self.encoder.config, "decoder":self.decoder.config}
+        print(self.config)
         self.pad_id = pad_id
         self.bos_id = bos_id
         self.eos_id = eos_id
--- chatbot_end2end/model/sentence_encoder.py
+++ chatbot_end2end/model/sentence_encoder.py
@@ -10,32 +10,33 @@ class SentenceEncoder(nn.Module):
             - bert_model_name: bert model file location or one of the supported model name
     '''
     def __init__(self, model_type="transformer", bert_model_name=None, vocab_size=30522,
-                 encoder_hidden_layers=12, encoder_attention_heads=12, max_position_embeddings=512,
-                 encoder_intermediate_size=3072, encoder_hidden_size=768, dropout=0.1, **args):
+                 encoder_hidden_layers=12, encoder_hidden_size=768, 
+                 encoder_intermediate_size=1024, encoder_attention_heads=12,
+                 max_position_embeddings=512,  dropout=0.1, **args):
         super().__init__()
         if model_type == "lstm":
             from nlptools.zoo.encoders.lstm import LSTMEncoder
             self.encoder = LSTMEncoder(vocab_size=vocab_size,
                                        hidden_size=encoder_hidden_size,
                                        num_hidden_layers=encoder_hidden_layers,
-                                       intermediate_size=encoder_intermediate_size,
                                        dropout=dropout)
+            self.config = self.encoder.config
         elif bert_model_name is not None:
-            from nlptools.zoo.encoders.transformer import TransformerEncoder
-            self.encoder = TransformerEncoder.from_pretrained(bert_model_name)
+            from pytorch_pretrained_bert.modeling import BertModel
+            self.encoder = BertModel.from_pretrained(bert_model_name)
             self.bert_model_name = bert_model_name
+            self.config = self.encoder.config.to_dict()
         else:
             from nlptools.zoo.encoders.transformer import TransformerEncoder
             self.encoder = TransformerEncoder(vocab_size=vocab_size,
                                               num_hidden_layers=encoder_hidden_layers,
                                               num_attention_heads=encoder_attention_heads,
                                               max_position_embeddings=max_position_embeddings,
-                                              intermediate_size=encoder_intermediate_size,
                                               hidden_size=encoder_hidden_size,
+                                              intermediate_size=encoder_intermediate_size,
                                               dropout=dropout)
-        self.config = self.encoder.config
+            self.config = self.encoder.config
         self.embedding = self.encoder.embeddings
-        self.hidden_size = self.config["hidden_size"]
 
     def freeze(self):
         """
