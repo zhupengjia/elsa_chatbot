dd70fb735bd4f08d11fe079dec3ecaa882a641ed
Pengjia Zhu <zhupengjia@gmail.com>
2019-07-17 19:54:11 +1200

    updated telegram to support voice

--- elsabot/backend/telegram_backend.py
+++ elsabot/backend/telegram_backend.py
@@ -1,25 +1,97 @@
 #!/usr/bin/env python
+import random, os, re
 from .backend import BackendBase
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from nlptools.audio.speech import Speech_Deepspeech
 
 class TelegramBackend(BackendBase):
-    def __init__(self, session_config, token, **args):
+    def __init__(self, session_config, token, deepspeech=None, tts=None, **args):
         super().__init__(session_config=session_config, **args)
         self.updater = Updater(token=token, use_context=True)
         self.dispatcher = self.updater.dispatcher
         self.dispatcher.add_handler(CommandHandler("start", TelegramBackend.start))
         self.dispatcher.add_handler(CommandHandler("reset", self.reset))
+        self.dispatcher.add_handler(CommandHandler("tts", self.get_tts))
         self.dispatcher.add_handler(MessageHandler(Filters.text, self.query))
+        self.dispatcher.add_handler(MessageHandler(Filters.voice, self.voice))
+        if deepspeech:
+            self.ds_model = Speech_Deepspeech(**deepspeech)
+        else:
+            self.ds_model = None
+        if tts:
+            from nlptools.audio.tts import MozillaTTS
+            self.tts_model = MozillaTTS(**tts)
+        else:
+            self.tts_model = None
 
     @staticmethod
     def start(update, context):
         chat_id = update.message.chat_id
         context.bot.send_message(chat_id=chat_id, text="Elsa is here ~")
-    
+
     def reset(self, update, context):
         chat_id = update.message.chat_id
         self.init_session()
         context.bot.send_message(chat_id=chat_id, text="Chatbot Reset")
+
+    def get_tts(self, update, context):
+        chat_id = update.message.chat_id
+        text = update.message.text.strip()
+        text = re.split("\s", text, maxsplit=1)[1]
+
+        random_id = random.randint(0,100000)
+        ogg_file = "/tmp/{}_{}.ogg".format(chat_id, random_id)
+        wav_file = "/tmp/{}_{}.wav".format(chat_id, random_id)
+
+        if self.tts_model:
+            self.tts_model(text, wav_file)
+            Speech_Deepspeech.wav2ogg(wav_file, ogg_file)
+            with open(ogg_file, "rb") as oggf:
+                context.bot.send_voice(chat_id=chat_id, voice=oggf)
+            os.remove(wav_file)
+            os.remove(ogg_file)
+        else:
+            context.bot.send_message(chat_id=chat_id, text=text)
+
+    def voice(self, update, context):
+        chat_id = update.message.chat_id
+        if not self.ds_model:
+            context.bot.send_voice(chat_id=chat_id, voice=update.message.voice)
+            return
+
+        #convert voice to text
+        random_id = random.randint(0,100000)
+        ogg_file = "/tmp/{}_{}.ogg".format(chat_id, random_id)
+        wav_file = "/tmp/{}_{}.wav".format(chat_id, random_id)
+        response_wave_file = "/tmp/response_{}_{}.wav".format(chat_id, random_id)
+        response_ogg_file = "/tmp/response_{}_{}.ogg".format(chat_id, random_id)
+
+        update.message.voice.get_file().download(ogg_file)
+        try:
+            Speech_Deepspeech.ogg2wav(ogg_file, wav_file)
+        except Exception as err:
+            context.bot.send_message(chat_id=chat_id, text=err)
+            os.remove(ogg_file)
+            os.remove(wav_file)
+            return
+
+        text = self.ds_model(wav_file)
+        os.remove(ogg_file)
+        os.remove(wav_file)
+
+        #query
+        response, score = self.session(text, session_id=chat_id)
+
+        #tts
+        if self.tts_model:
+            self.tts_model(response, response_wave_file)
+            Speech_Deepspeech.wav2ogg(response_wave_file, response_ogg_file)
+            with open(response_ogg_file, "rb") as oggf:
+                context.bot.send_voice(chat_id=chat_id, voice=oggf)
+            os.remove(response_wave_file)
+            os.remove(response_ogg_file)
+        else:
+            context.bot.send_message(chat_id=chat_id, text=response)
 
     def query(self, update, context):
         chat_id = update.message.chat_id
